caa4fd1f526f42056069a73e30fa5b833b1d6f4b
Raissa Costa <raissa.metal@gmail.com>
2017-06-21 11:59:09 -0300

    quem sou - so falta lógica php

---
+++ wp-content/themes/fernando/page-quem-sou.php
@@ -0,0 +1,87 @@
+<?php get_header(); ?>
+
+<section class="quem-sou">
+		
+	<section class="container-fluid title-quem-sou">
+		<div class="row">
+			<div class="container">
+				<div class="col-sm-offset-2 col-sm-8">
+					<h3>Lorem ipsum <span>adipisicing</span></h3>
+					<h6>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</h6>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="container-fluid timeline">
+		<div class="row">
+			
+			<ul class="col-sm-12">
+
+				<li class="row midia">
+					<div class="col-sm-offset-1 col-sm-5">
+						<figure><img src="<?php bloginfo('template_url') ?>/img/fernando-duarte-foto-quem-sou.jpg" alt=""></figure>
+					</div>
+
+					<div class="col-sm-1 pin"><div class="circle"></div></div>
+
+					<div class="col-sm-4">
+						<h2>2017</h2>
+						<h3>Lorem ipsum <span>adipisicing</span></h3>
+						<h6>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</h6>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+
+				</li>
+
+				<li class="row inverte">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-5 col-sm-push-7 ano">
+								<h2>2016</h2>
+							</div>
+
+							<div class="col-sm-1 col-sm-push-1 pin"><div class="circle"></div></div>
+
+							<div class="col-sm-6 col-sm-pull-6 txt">
+								<h3>Lorem ipsum <span>adipisicing</span></h3>
+								<h6>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</h6>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							</div>
+						</div>
+					</div>
+				</li>
+
+				<li class="row">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-5 ano">
+								<h2>2016</h2>
+							</div>
+
+							<div class="col-sm-1 pin"><div class="circle"></div></div>
+
+							<div class="col-sm-6 txt">
+								<h3>Lorem ipsum <span>adipisicing</span></h3>
+								<h6>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</h6>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							</div>
+						</div>
+					</div>
+				</li>
+
+
+			</ul>
+
+		</div>
+	</section>
+
+</section>
+
+
+
+
+
+<?php get_footer(); ?>
